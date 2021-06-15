2649f4f0faf31c8bacad69cc1e276d7b6f9cae37
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-15 17:51:13 +0530

    added:Employee Daily Wage

--- Emp.java
+++ Emp.java
@@ -1,9 +1,12 @@
 public class Emp{
 	public static void main(String [] args) {
 		int isFullTime = 1;
+		int wagePerHour = 20;
+		int fullDayWork = 8;
 		double value = Math.floor(Math.random() * 10) % 2;
 		if(value == isFullTime) {
 		System.out.println("Emp is Present");
+		System.out.println("Employee Daily Wage is: " +wagePerHour*fullDayWork);
 		}
 		else 
 		System.out.println("EMp is Absent");
