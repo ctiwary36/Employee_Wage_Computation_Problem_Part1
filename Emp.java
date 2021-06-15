c1c069dbbe467ff3fcd2e4b9066ebb1288ec8053
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-15 17:42:05 +0530

    Added:UC1 Employee Present or Absent

--- Emp.java
+++ Emp.java
@@ -1,5 +1,12 @@
 public class Emp{
-	public static void main(String[] args){
-	System.out.println("Welcome to Employee Wages Computation Problem"); 
-	}
+	public static void main(String [] args) {
+		int isFullTime = 1;
+		double value = Math.floor(Math.random() * 10) % 2;
+		if(value == isFullTime) {
+		System.out.println("Emp is Present");
+		}
+		else 
+		System.out.println("EMp is Absent");
+		
+}
 }
