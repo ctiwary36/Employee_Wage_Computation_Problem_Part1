606eee8bd796be027b1d87e5078ef9b69553c196
Chandan Tiwary <ctiwary36@gmail.com>
2021-06-24 22:48:11 +0530

    AddedCompute Employee Wage for multiple companies

--- Emp.java
+++ Emp.java
@@ -1,10 +1,16 @@
 package com.Employee;
 
 public class Emp {
-
+    //Main Method
         public static void main(String[] args) {
+
+            //Creating object for Empwage
             EmpWage obj =  new EmpWage();
-            obj.calculateWage();
+
+        //calling calculateWage method using object
+            obj.calculateWage("FlipKart", 50, 20, 200);
+            obj.calculateWage("Amazon", 25, 25, 250);
+
 
         }
     }
--- EmpWage.java
+++ EmpWage.java
@@ -2,47 +2,54 @@ package com.Employee;
 
 public class EmpWage {
 
-//Method for calculating wages
-//using while loop for condition check
+    //Method for calculating wages
+  //using while loop for condition check
+        public static void calculateWage(String companyName, int wagePerHr, int maxWorkingDays, int maxWorkingHrs) {
 
-        public static void calculateWage() {
 
-    //declaring variables
-            int FullDayHr=8;
-            int PartTimeHr=4;
-            int WagePerHr=20;
+            int FULL_DAYHR=8;
+            int PART_TIMEHR=4;
             int day=0;
             int empHr=0;
             int wage = 0;
-            //
+            //[printing  company details
+            System.out.println("Details of " + companyName + " employee");
+            System.out.println("|.......................................|");
+            System.err.println("Wage per hour:" + wagePerHr);
+            System.out.println("Maximum working days:" + maxWorkingDays);
+            System.out.println("Maximum working hours:" + maxWorkingHrs);
+
             while (day <= 19) {
                 int n = (int) (Math.random() * 10) % 3;
                 switch (n) {
                     case 1:
-                        System.out.println("Present Fullday");
-                        wage = (wage + (WagePerHr * FullDayHr));
-                        empHr=(empHr+ FullDayHr);
+                        System.out.println("Present Full day");
+                        wage = (wage + (maxWorkingHrs * FULL_DAYHR));
+
+                        empHr=(empHr+ FULL_DAYHR);
                         day++;
                         break;
                     case 2:
                         System.out.println("Present PartTime");
-                        wage = (wage + (WagePerHr * PartTimeHr));
-                        empHr=(empHr+ PartTimeHr);
+                        wage = (wage + (maxWorkingHrs * PART_TIMEHR));
+
+                        empHr=(empHr+ PART_TIMEHR);
 
                         day++;
                         break;
                     default:
-                        System.out.println("absent");
+                        System.out.println("Absent");
                         day++;
                         if( empHr == 100)
                             break;
                 }
             }
             //Printing wages
-            System.out.println("total wage of a month is: " +wage);
+            System.out.println("Total Wage of a month is: " +wage);
 
             System.out.println("Employee Hour: "+empHr);
-            System.out.println("Employee Work Day: "+day);
+            System.out.println("Employee Work Day: "+day + "\n");
 
         }
-}
+
+    }
